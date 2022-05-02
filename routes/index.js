58dda4cb7eb3266be62746b8bbee07679abee072
Korey Luu <39642290+Ahrionic@users.noreply.github.com>
2022-05-02 02:57:08 -0700

    clean up

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,5 @@ const router = require('express').Router()
 
 router.use('/api', require('./userRoutes.js'))
 router.use('/api', require('./postRoutes.js'))
-// other routers go here...
 
 module.exports = router
